631a9f90c82095ed50a2b425213228e7ef9ee36c
Asjad Mehmood <137377769+AsjadMehmood@users.noreply.github.com>
2025-02-18 21:19:40 +0500

    Update Woocommerce Order Status Change to Completed.php

--- Woocommerce Order Status Change to Completed.php
+++ Woocommerce Order Status Change to Completed.php
@@ -1,12 +1,12 @@
 /**
- * Automatically mark all WooCommerce orders as "Completed" regardless of their initial status.
+ * Automatically mark all "Processing" orders as "Completed".
  */
-add_action('woocommerce_order_status_changed', 'auto_complete_all_orders', 10, 4);
+add_action('woocommerce_order_status_processing', 'auto_complete_all_processing_orders');
 
-function auto_complete_all_orders($order_id, $old_status, $new_status, $order) {
-    // Check if the order is not already completed
-    if ($new_status !== 'completed') {
-        // Update the order status to "Completed"
-        $order->update_status('completed');
-    }
+function auto_complete_all_processing_orders($order_id) {
+    // Get the order object
+    $order = wc_get_order($order_id);
+
+    // Update the order status to "Completed"
+    $order->update_status('completed');
 }
